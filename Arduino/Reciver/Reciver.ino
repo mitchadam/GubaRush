73cc802d284a94bc40e1b0bfa91801a0bce44082
ryan <4721319+rpshukla@users.noreply.github.com>
2018-11-18 01:36:32 -0700

    Receive and send gyro data on Reciver

--- Arduino/Reciver/Reciver.ino
+++ Arduino/Reciver/Reciver.ino
@@ -16,19 +16,22 @@ void setup()
 
 void loop()
 {
-    char buf[6]; // will store raw data read from driver
+    char buf[12]; // will store raw data read from driver
     uint8_t buflen = sizeof(buf);
     
-    int16_t* integers; // will
+    int16_t* integers; // will store data converted to int16's
 
     // read raw data into buf
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
       integers = (int16_t*) buf; // point integers at buf so that the data is read as int16's 
       Serial.println("start");
-      Serial.println(integers[0]);  
-      Serial.println(integers[1]);  
-      Serial.println(integers[2]);
+      Serial.println(integers[0]); // AcX  
+      Serial.println(integers[1]); // AcY
+      Serial.println(integers[2]); // AcZ
+      Serial.println(integers[3]); // GyX
+      Serial.println(integers[4]); // GyY
+      Serial.println(integers[5]); // GyZ
       Serial.println("end");      
     }
 }
